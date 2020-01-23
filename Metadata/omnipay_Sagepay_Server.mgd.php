7714e9edc4503c8726d34d7a35a0f94659bc094f
Carlos Capote <carloscapote@thirdsectordesign.org>
2020-01-23 11:12:25 +0000

    Declare notification from different session as processor metadata

--- Metadata/omnipay_Sagepay_Server.mgd.php
+++ Metadata/omnipay_Sagepay_Server.mgd.php
@@ -74,6 +74,7 @@ return [
       'ipn_processing_delay' => 0,
       // See https://github.com/thephpleague/omnipay-sagepay/pull/154
       'pass_through_fields' => ['billingForShipping' => 1],
+      'notification_from_different_session' => TRUE,
     ],
     'params' =>
       [
@@ -86,6 +87,7 @@ return [
         'class_name' => 'Payment_OmnipayMultiProcessor',
         'billing_mode' => 4,
         'payment_type' => 3,
+        'is_recur' => 1,
       ],
   ],
 ];
