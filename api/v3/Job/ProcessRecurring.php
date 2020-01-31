6629da8bc276da81714ee57d6dfd853ea534d1ae
Carlos Capote <carlos.capote@hawara.es>
2020-01-31 14:03:37 +0000

    Supports continuous authority when processing recurring payments

--- api/v3/Job/ProcessRecurring.php
+++ api/v3/Job/ProcessRecurring.php
@@ -35,7 +35,7 @@ function civicrm_api3_job_process_recurring($params) {
         'is_email_receipt' => FALSE,
       ]);
 
-      $payment = civicrm_api3('PaymentProcessor', 'pay', [
+      $paymentParams = [
         'amount' => $originalContribution['total_amount'],
         'currency' => $originalContribution['currency'],
         'payment_processor_id' => $paymentProcessorID,
@@ -43,12 +43,22 @@ function civicrm_api3_job_process_recurring($params) {
         'contribution_id' => $pending['id'],
         'contactID' => $originalContribution['contact_id'],
         'description' => ts('Repeat payment, original was ' . $originalContribution['id']),
-        'token' => civicrm_api3('PaymentToken', 'getvalue', [
+        'payment_action' => 'purchase',
+      ];
+
+      $paymentProcessor = $omnipayProcessors['values'][$paymentProcessorID];
+
+      if (!is_continuous_authority($paymentProcessor['payment_processor_type_id'])) {
+        $paymentParams['token'] = civicrm_api3('PaymentToken', 'getvalue', [
           'id' => $recurringPayment['payment_token_id'],
           'return' => 'token',
-        ]),
-        'payment_action' => 'purchase',
-      ]);
+        ]);
+      } else {
+        $paymentParams['original_contribution_trxn_id'] = $originalContribution['trxn_id'];
+        $paymentParams['continuous_authority_repeat'] = true;
+      }
+
+      $payment = civicrm_api3('PaymentProcessor', 'pay', $paymentParams);
       $payment = reset($payment['values']);
 
       civicrm_api3('Contribution', 'completetransaction', [
@@ -57,7 +67,6 @@ function civicrm_api3_job_process_recurring($params) {
         'payment_processor_id' => $paymentProcessorID,
       ]);
       $result['success']['ids'] = $recurringPayment['id'];
-
     }
     catch (CiviCRM_API3_Exception $e) {
       // Failed - what to do?
@@ -86,4 +95,88 @@ function civicrm_api3_job_process_recurring($params) {
  * @return array
  */
 function _civicrm_api3_job_process_recurring_spec(&$params) {
+}
+
+/**
+ * Checks if a given payment processor type is configured to work as
+ * a continuous authority.
+ *
+ * @param array $processorTypeId Id of the processor type
+ *
+ * @return boolean
+ */
+function is_continuous_authority($processorTypeId) {
+  $paymentProcessorType = civicrm_api3('PaymentProcessorType', 'get', array(
+    'id' => $processorTypeId,
+    'sequential' => 1,
+  ));
+
+  $property = get_processor_type_property(
+    $paymentProcessorType['values'][0]['name'],
+    'continuous_authority'
+  );
+
+  if($property['found']) {
+    return $property['value'] == true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Gets the value of a property for a given payment processor type.
+ *
+ * @param string $processorTypeName Name of the payment processor type
+ * @param string $propertyName Property name
+ *
+ * @return array Array containing a boolean property `found`
+ *    with a false value if the property isn't defined for
+ *    the given processor type, or true otherwise.
+ *    If the property is defined, the array will also contain
+ *    its `value`
+ */
+function get_processor_type_property($processorTypeName, $propertyName) {
+  $processorTypeMetadata = get_processor_type_metadata($processorTypeName);
+  if(!array_key_exists('metadata', $processorTypeMetadata)) {
+    return array('found' => false);
+  }
+  if(!array_key_exists($propertyName, $processorTypeMetadata['metadata'])) {
+    return array('found' => false);
+  }
+  return array(
+    'found' => true,
+    'value' => $processorTypeMetadata['metadata'][$propertyName],
+  );
+}
+
+/**
+ * Get the metadata associated with a processor type.
+ *
+ * @param string $processorTypeName Name of the payment processor type
+ *
+ * @return mixed Metadata for the given payment processor type
+ */
+function get_processor_type_metadata($processorTypeName) {
+  $processors = [];
+  omnipaymultiprocessor_civicrm_managed($processors);
+  $filter_by_name = function($processor) use($processorTypeName) {
+    return filter_processor_type_by_name($processorTypeName, $processor);
+  };
+  return array_values(array_filter($processors, $filter_by_name))[0];
+}
+
+/**
+ * Returns true if a managed entity represents a processor type with
+ * a given name.
+ *
+ * @param string $processorTypeName Name of the payment processor type
+ * @param array $processor Managed entity being evaluated
+ *
+ * @return boolean
+ */
+function filter_processor_type_by_name($processorTypeName, $processor) {
+  if($processor['entity'] != 'payment_processor_type') { return false; }
+  if(!array_key_exists('params', $processor)) { return false; }
+  if(!array_key_exists('name', $processor['params'])) { return false; }
+  return $processor['params']['name'] == $processorTypeName;
 }
